f1d77588b2ceb9ef41d8eba6681f8a2f9ec3af16
Allister Terhorst Ramos <allistertr@hotmail.com>
2018-07-24 07:24:26 -0300

    feat: Added to internationalization

--- src/App.js
+++ src/App.js
@@ -1,20 +1,72 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import languages from './languages.json';
+import * as BooksAPI from './BooksAPI'
+import { prototype } from 'react-transition-group/TransitionGroup';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { translate: languages['en'] }
+
+    this.languagesLabels = Object.keys(languages).map(prototype => ({ key: prototype, label: languages[prototype].LANGUAGE_LABEL }));
+    console.log(this.languagesLabels)
+    BooksAPI.getAll()
+      .then(books => console.log(books))
+    console.log(BooksAPI)
+  }
+
+  changeLanguage = language => {
+    this.setState({ translate: languages[language] });
+  };
+
   render() {
+    let { translate } = this.state;
     return (
       <div>
-        <Route exact path="/" render={() => (
+        <TextField
+          id="select-currency"
+          select
+
+
+          value={this.state.translate.LANGUAGE_KEY}
+          onChange={(event) => this.changeLanguage(event.target.value)}
+
+
+          margin="normal"
+        >
+          {this.languagesLabels.map(option => (
+            <MenuItem key={option.key} value={option.key}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <input
+          accept=".json"
+
+          id="contained-button-file"
+          multiple
+          type="file"
+        />
+        <label htmlFor="contained-button-file">
+          <Button variant="contained" component="span" >
+            Upload
+        </Button>
+        </label>
+        <Route exact path="/" render={({ history }) => (
           <div>
             <p>
-              Hello word
-              </p>
+              {translate.HELLO_WORLD}
+            </p>
             <Link
               to="/search"
             >Search</Link>
-            <Button variant="contained" color="primary" href="/search">
+            <Button variant="contained" color="primary" onClick={() => history.push('/search')}>
               Test
             </Button>
           </div>
