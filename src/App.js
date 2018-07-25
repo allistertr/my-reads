73bb24c51d95d53fe2f1ae16548db1403735d7c1
Allister Terhorst Ramos <allistertr@hotmail.com>
2018-07-25 07:17:29 -0300

    feat: shelf implementation and book style initialization

--- src/App.js
+++ src/App.js
@@ -3,15 +3,39 @@ import { Route, Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import languages from './languages.json';
 import * as BooksAPI from './BooksAPI'
 import { prototype } from 'react-transition-group/TransitionGroup';
+
+import Shelves from './Shelves';
 
 class App extends Component {
 
   constructor() {
     super();
-    this.state = { translate: languages['en'] }
+    this.state = {
+      translate: languages['en'],
+      books: [
+        {
+          name: 'Test None',
+          shelf: 'none'
+        },
+        {
+          name: 'Test currentlyReading',
+          shelf: 'currentlyReading'
+        },
+        {
+          name: 'Test read',
+          shelf: 'read'
+        },
+        {
+          name: 'Test wantToRead',
+          shelf: 'wantToRead'
+        }
+      ]
+    };
 
     this.languagesLabels = Object.keys(languages).map(prototype => ({ key: prototype, label: languages[prototype].LANGUAGE_LABEL }));
     console.log(this.languagesLabels)
@@ -25,39 +49,46 @@ class App extends Component {
   };
 
   render() {
-    let { translate } = this.state;
+    let { translate, books } = this.state;
     return (
       <div>
-        <TextField
-          id="select-currency"
-          select
-
-
-          value={this.state.translate.LANGUAGE_KEY}
-          onChange={(event) => this.changeLanguage(event.target.value)}
-
-
-          margin="normal"
+        <Grid
+          container
+          // spacing={16}
+          // alignItems={alignItems}
+          direction='column'
+        // justify={justify}
         >
-          {this.languagesLabels.map(option => (
-            <MenuItem key={option.key} value={option.key}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+          <Shelves books={books} translate={translate} />
+          <TextField
+            id="select-currency"
+            select
+            value={this.state.translate.LANGUAGE_KEY}
+            onChange={(event) => this.changeLanguage(event.target.value)}
+            margin="normal"
+          >
+            {this.languagesLabels.map(option => (
+              <MenuItem key={option.key} value={option.key}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
 
-        <input
-          accept=".json"
-
-          id="contained-button-file"
-          multiple
-          type="file"
-        />
-        <label htmlFor="contained-button-file">
-          <Button variant="contained" component="span" >
-            Upload
-        </Button>
-        </label>
+          <input
+            accept=".json"
+            style={{
+              display: 'none',
+            }}
+            id="contained-button-file"
+            multiple
+            type="file"
+          />
+          <label htmlFor="contained-button-file">
+            <Button color="primary" variant="contained" component="span" >
+              Upload
+            </Button>
+          </label>
+        </Grid>
         <Route exact path="/" render={({ history }) => (
           <div>
             <p>
---
+++ src/Shelves.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Book from './Book';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  grid: {
+    flexGrow: 1,
+  }
+});
+
+function Shelves(props) {
+  const { classes, books, translate } = props;
+  let shelves = [
+    {
+      name: 'currentlyReading',
+      label: translate.CURRENTLY_READING
+    },
+    {
+      name: 'read',
+      label: translate.READ
+    },
+    {
+      name: 'wantToRead',
+      label: translate.WANT_TO_READ
+    }
+  ];
+
+  shelves.forEach(shelf => {
+    shelf.books = books.filter(book => book.shelf === shelf.name);
+  });
+
+  return (
+    <div className={classes.root}>
+      {shelves.map(shelf => (
+        <div>
+          <p>{shelf.label}</p>
+          <Grid container spacing={24}>
+              {shelf.books.map(book => (
+                <Grid item key={book.name}>
+                  <Book book={book} />
+                </Grid>
+              ))}
+          </Grid>
+        </div>
+      ))}
+        {/* <Grid item xs={12}> */}
+
+        {/* <Divider />
+        <Divider light /> */}
+    </div>
+  );
+}
+
+Shelves.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Shelves);
